e9cd462d57f281646c777169dfd265c2e2449285
Rishikesh Reddy <rishikeshreddymamilla@gmail.com>
2026-03-06 00:54:57 -0500

    fix: add missing authMiddleware file

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const prisma = require('../prisma');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../Middleware/authMiddleware');
 
 // GET all companies - public
 router.get('/', async (req, res) => {
