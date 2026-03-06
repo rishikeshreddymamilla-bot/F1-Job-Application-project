67fc1daa0c5f8f839f663c090b5f54bff8131c75
Rishikesh Reddy <rishikeshreddymamilla@gmail.com>
2026-03-06 01:04:41 -0500

    fix: rename Middleware to lowercase middleware

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const prisma = require('../prisma');
-const authMiddleware = require('../Middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // GET all companies - public
 router.get('/', async (req, res) => {
